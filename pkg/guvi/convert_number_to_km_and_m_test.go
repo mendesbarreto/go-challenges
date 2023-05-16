0541da6cab845634c41846c0cd135eabb9d14bcf
Douglas <mendes-barreto@Live.com>
2023-05-16 10:40:15 -0400

    chore: Add live reload for tests

---
+++ pkg/guvi/convert_number_to_km_and_m_test.go
@@ -0,0 +1,7 @@
+package guvi
+
+//You are given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres.
+//Input Description:
+//A number "A" representing some distance in kilometer is provided to you as the input.
+//Output Description:
+//Convert and print this value in meters and centimeters.
