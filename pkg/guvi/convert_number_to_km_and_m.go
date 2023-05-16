8995f2cf13f1f79004470e88134c4188fc7b34ff
Douglas <mendes-barreto@Live.com>
2023-05-16 10:57:29 -0400

    chore: prepare code to unit tests

--- pkg/guvi/convert_number_to_km_and_m.go
+++ pkg/guvi/convert_number_to_km_and_m.go
@@ -1,7 +1,23 @@
 package guvi
 
+import "fmt"
+
 //You are given a number A in Kilometers. Convert this into B: Meters and C: Centi-Metres.
 //Input Description:
 //A number "A" representing some distance in kilometer is provided to you as the input.
 //Output Description:
 //Convert and print this value in meters and centimeters.
+//
+
+func ConvertKilometersToMeters(kilometers float32) float32 {
+	return kilometers * 1000
+}
+
+func ConvertKilometersToCentimeters(kilometers float32) float32 {
+	return kilometers * 100000
+}
+
+func PrintResult(kilometers float32) {
+	fmt.Println(ConvertKilometersToMeters(kilometers))
+	fmt.Println(ConvertKilometersToCentimeters(kilometers))
+}
